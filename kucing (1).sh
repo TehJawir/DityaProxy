2408fc72552cf727fd4c147a155e35128303b592
TehJawir <146950373+TehJawir@users.noreply.github.com>
2023-10-04 19:07:07 +0700

    Add files via upload

---
+++ kucing (1).sh
@@ -0,0 +1,26 @@
+RED="\e[1;31m"
+GREEN="\e[1;32m"
+ENDCOLOR="\e[0m"
+
+clear
+echo -e "${GREEN}Installing Proxy...${ENDCOLOR}"
+sleep 1
+if [ -f "arunika" ]; then
+    echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
+    rm arunika
+    sleep 1
+    echo -e "${GREEN}Updating proxy...${ENDCOLOR}"
+fi
+echo -e "${GREEN}Checking Termux Installer..."
+pkg update
+pkg upgrade
+pkg install openssl
+pkg install curl
+pkg install libenet
+pkg install wget
+clear
+wget -q https://github.com/AmertaByte/Arunika-Proxy/raw/main/arunika
+sleep 1
+echo -e "${GREEN}Proxy Installed${ENDCOLOR}"
+echo -e "${GREEN}Execute proxy with this command: ./arunika${ENDCOLOR}"
+chmod +x arunika
